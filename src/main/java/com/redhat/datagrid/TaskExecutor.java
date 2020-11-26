67ae7eea80668ce46999de686ae0e85a371c960e
Tristan Tarrant <ttarrant@redhat.com>
2020-11-26 14:02:01 +0100

    Make it work with Infinispan 8.2

--- src/main/java/com/redhat/datagrid/TaskExecutor.java
+++ src/main/java/com/redhat/datagrid/TaskExecutor.java
@@ -2,7 +2,6 @@ package com.redhat.datagrid;
 
 import java.util.Collections;
 
-import org.infinispan.client.hotrod.ProtocolVersion;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 
@@ -18,7 +17,7 @@ public class TaskExecutor {
       }
       System.out.printf("Running task %s on cache %s on server %s:%s", args[3], args[2], args[0], args[1]);
       ConfigurationBuilder builder = new ConfigurationBuilder();
-      builder.addServer().host(args[0]).port(Integer.parseInt(args[1])).version(ProtocolVersion.PROTOCOL_VERSION_28);
+      builder.addServer().host(args[0]).port(Integer.parseInt(args[1])).protocolVersion("2.6");
       RemoteCacheManager rcm = new RemoteCacheManager(builder.build());
       rcm.getCache(args[2]).execute(args[3], Collections.emptyMap());
       System.out.println("Done.");
